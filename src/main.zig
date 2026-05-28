0d89d0647129e798ee2d948ca8f1e5573c776fbf
RonnyLP <2223010014@untels.edu.pe>
2026-05-28 07:14:44 -0500

    Basic zig input/output

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,20 @@
 const std = @import("std");
 
 pub fn main(init: std.process.Init) !void {
-    var stdout = std.Io.File.stdout().writer(init.io, &.{});
+    var out_buf: [1024]u8 = undefined;
+    var in_buf: [1024]u8 = undefined;
+    var stdout = std.Io.File.stdout().writer(init.io, &out_buf);
+    var stdin = std.Io.File.stdin().reader(init.io, &in_buf);
+    var stdout_interface = &stdout.interface;
+    var stdin_interface = &stdin.interface;
+
+    stdin_interface = stdin_interface;
 
     // TODO: Uncomment the code below to pass the first stage
-    try stdout.interface.print("$ ", .{});
+    try stdout_interface.print("$ ", .{});
+    try stdout_interface.flush();
+
+    const cmd = try stdin_interface.takeDelimiter('\n') orelse return;
+    try stdout_interface.print("{s}: command not found\n", .{cmd});
+    try stdout_interface.flush();
 }
