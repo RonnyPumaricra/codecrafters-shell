e26d820b86bda11f906dd0c49c0ad366bc3316fe
RonnyLP <2223010014@untels.edu.pe>
2026-06-04 15:00:11 -0500

    Type checks for executables in path

--- src/main.zig
+++ src/main.zig
@@ -10,6 +10,10 @@ pub fn main(init: std.process.Init) !void {
 
     stdin_interface = stdin_interface;
 
+    const io = init.io;
+
+    const PATH = init.minimal.environ.getPosix("PATH").?;
+
     // REPL: Read-Eval-Print-Loop
     while (true) {
         try stdout_interface.print("$ ", .{});
@@ -35,8 +39,27 @@ pub fn main(init: std.process.Init) !void {
                     try stdout_interface.print("{s} is a shell builtin\n", .{cmd[5..]});
                     try stdout_interface.flush();
                 } else {
-                    try stdout_interface.print("{s}: not found\n", .{cmd[5..]});
-                    try stdout_interface.flush();
+                    var path_it = std.mem.splitAny(u8, PATH, ":");
+
+                    while (path_it.next()) |single_path| {
+                        const d = try std.Io.Dir.openDirAbsolute(io, single_path, .{
+                            .access_sub_paths = true,
+                            .iterate = true,
+                        });
+                        defer d.close(io);
+                        const st = d.statFile(io, cmd[5..], .{}) catch continue;
+
+                        // Permisos 3 bit: 4: read 2: write 1: execute
+                        const owner_permissions = @intFromEnum(st.permissions) % 0o1000 / 0o100;
+                        if (owner_permissions % 2 == 1) {
+                            try stdout_interface.print("{s} is {s}/{s}\n", .{ cmd[5..], single_path, cmd[5..] });
+                            try stdout_interface.flush();
+                            break;
+                        }
+                    } else {
+                        try stdout_interface.print("{s}: command not found\n", .{cmd[5..]});
+                        try stdout_interface.flush();
+                    }
                 }
             }
         } else {
