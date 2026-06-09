623f9a66e0fe48d8b480dc04676e4f21b42905e6
RonnyLP <2223010014@untels.edu.pe>
2026-06-09 11:35:33 -0500

    Modularized builtin functions out of main

---
+++ src/builtins.zig
@@ -0,0 +1,98 @@
+const std = @import("std");
+const Io = std.Io;
+
+pub const ShellContext = struct {
+    io: std.Io,
+
+    cmd: []const u8,
+    PATH: []const u8,
+    stdout: *Io.Writer,
+    should_quit: *bool,
+};
+
+pub const Command = struct {
+    name: []const u8,
+    func: *const fn (ShellContext) anyerror!void,
+};
+
+pub const all_commands = [_]Command{
+    .{
+        .name = "echo",
+        .func = echo_command,
+    },
+    .{
+        .name = "type",
+        .func = type_command,
+    },
+    .{
+        .name = "exit",
+        .func = exit_command,
+    },
+};
+
+pub fn execute_command(shell_context: ShellContext) !void {
+    for (all_commands) |command| {
+        if (std.mem.startsWith(u8, shell_context.cmd, command.name)) {
+            try command.func(shell_context);
+            return;
+        }
+    }
+    return error.CommandNotFound;
+}
+
+pub fn echo_command(shell_context: ShellContext) !void {
+    const cmd = shell_context.cmd;
+    const stdout = shell_context.stdout;
+    if (cmd.len >= 5) {
+        try stdout.print("{s}\n", .{cmd[5..]});
+        try stdout.flush();
+    }
+}
+
+fn find_executable(io: Io, file: []const u8, PATH: []const u8) !?[]const u8 {
+    var path_it = std.mem.splitAny(u8, PATH, ":");
+
+    while (path_it.next()) |single_path| {
+        const d = try std.Io.Dir.openDirAbsolute(io, single_path, .{
+            .access_sub_paths = true,
+            .iterate = true,
+        });
+        defer d.close(io);
+        const st = d.statFile(io, file, .{}) catch continue;
+
+        // Permisos 3 bit: 4: read 2: write 1: execute
+        const owner_permissions = @intFromEnum(st.permissions) % 0o1000 / 0o100;
+        if (owner_permissions % 2 == 1) {
+            return single_path;
+        }
+    }
+    return null;
+}
+
+pub fn type_command(shell_context: ShellContext) !void {
+    const io = shell_context.io;
+    const PATH = shell_context.PATH;
+    const cmd = shell_context.cmd;
+    const stdout = shell_context.stdout;
+    if (cmd.len >= 5) {
+        for (all_commands) |builtin_command| {
+            if (std.mem.eql(u8, builtin_command.name, cmd[5..])) {
+                try stdout.print("{s} is a shell builtin\n", .{cmd[5..]});
+                try stdout.flush();
+                break;
+            }
+        } else {
+            if (try find_executable(io, cmd[5..], PATH)) |exec_dir_path| {
+                try stdout.print("{s} is {s}/{s}\n", .{ cmd[5..], exec_dir_path, cmd[5..] });
+                try stdout.flush();
+            } else {
+                try stdout.print("{s}: not found\n", .{cmd[5..]});
+                try stdout.flush();
+            }
+        }
+    }
+}
+
+pub fn exit_command(shell_context: ShellContext) !void {
+    shell_context.should_quit.* = true;
+}
--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const builtins = @import("builtins.zig");
 
 pub fn main(init: std.process.Init) !void {
     var out_buf: [1024]u8 = undefined;
@@ -8,87 +9,41 @@ pub fn main(init: std.process.Init) !void {
     var stdout_interface = &stdout.interface;
     var stdin_interface = &stdin.interface;
 
-    stdin_interface = stdin_interface;
-
     const io = init.io;
 
     const PATH = init.minimal.environ.getPosix("PATH").?;
+    var should_quit = false;
 
     // REPL: Read-Eval-Print-Loop
-    while (true) {
+    while (!should_quit) {
         try stdout_interface.print("$ ", .{});
         try stdout_interface.flush();
 
         const cmd = try stdin_interface.takeDelimiter('\n') orelse return;
 
-        if (std.mem.eql(u8, "exit", cmd)) {
-            break;
-        }
+        builtins.execute_command(.{
+            .io = io,
+            .PATH = PATH,
+            .stdout = stdout_interface,
+            .cmd = cmd,
+            .should_quit = &should_quit,
+        }) catch {
+            var argv: std.ArrayList([]const u8) = try .initCapacity(init.gpa, 64);
+            defer argv.deinit(init.gpa);
 
-        if (std.mem.startsWith(u8, cmd, "echo")) {
-            if (cmd.len >= 5) {
-                try stdout_interface.print("{s}\n", .{cmd[5..]});
-                try stdout_interface.flush();
+            var argv_it = std.mem.splitAny(u8, cmd, " ");
+            while (argv_it.next()) |arg| {
+                _ = try argv.append(init.gpa, arg);
             }
-        } else if (std.mem.startsWith(u8, cmd, "type")) {
-            if (cmd.len >= 5) {
-                if (std.mem.eql(u8, "echo", cmd[5..]) or
-                    std.mem.eql(u8, "type", cmd[5..]) or
-                    std.mem.eql(u8, "exit", cmd[5..]))
-                {
-                    try stdout_interface.print("{s} is a shell builtin\n", .{cmd[5..]});
-                    try stdout_interface.flush();
-                } else {
-                    var path_it = std.mem.splitAny(u8, PATH, ":");
 
-                    while (path_it.next()) |single_path| {
-                        const d = try std.Io.Dir.openDirAbsolute(io, single_path, .{
-                            .access_sub_paths = true,
-                            .iterate = true,
-                        });
-                        defer d.close(io);
-                        const st = d.statFile(io, cmd[5..], .{}) catch continue;
-
-                        // Permisos 3 bit: 4: read 2: write 1: execute
-                        const owner_permissions = @intFromEnum(st.permissions) % 0o1000 / 0o100;
-                        if (owner_permissions % 2 == 1) {
-                            try stdout_interface.print("{s} is {s}/{s}\n", .{ cmd[5..], single_path, cmd[5..] });
-                            try stdout_interface.flush();
-                            break;
-                        }
-                    } else {
-                        try stdout_interface.print("{s}: not found\n", .{cmd[5..]});
-                        try stdout_interface.flush();
-                    }
-                }
-            }
-        } else {
-            var path_it = std.mem.splitAny(u8, PATH, ":");
-
-            while (path_it.next()) |single_path| {
-                const d = try std.Io.Dir.openDirAbsolute(io, single_path, .{
-                    .access_sub_paths = true,
-                    .iterate = true,
-                });
-                defer d.close(io);
-
-                var argv: std.ArrayList([]const u8) = try .initCapacity(init.gpa, 64);
-                defer argv.deinit(init.gpa);
-
-                var argv_it = std.mem.splitAny(u8, cmd, " ");
-                while (argv_it.next()) |arg| {
-                    _ = try argv.append(init.gpa, arg);
-                }
-
-                var child = std.process.spawn(io, .{
-                    .argv = argv.items,
-                }) catch continue;
-                _ = try child.wait(io);
-                break;
-            } else {
+            var child = std.process.spawn(io, .{
+                .argv = argv.items,
+            }) catch {
                 try stdout_interface.print("{s}: not found\n", .{cmd});
                 try stdout_interface.flush();
-            }
-        }
+                continue;
+            };
+            _ = try child.wait(io);
+        };
     }
 }
